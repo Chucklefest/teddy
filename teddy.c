021b913f5ccddda3832ab3948059b964fb3527b4
pierre <tiarnachreidy@gmail.com>
2017-07-26 13:17:44 +0100

    Up and Down scrolling

--- teddy.c
+++ teddy.c
@@ -12,27 +12,48 @@ typedef struct line {
 	struct line * next;
 } line_t;
 line_t * cur;
+line_t * root;
+line_t * tail;
 
 int readline(char buf[], FILE * fp)
 {
 	return fgets(buf, LINE_BUF_LEN, fp) != NULL ? 1 : 0;
 }
-int scroll(char down, FILE * fp)
+int loadfile(FILE * fp)
 {
-	if(down)
-	{ 
+	while(tail == (line_t *) NULL)
+	{
 		cur->next = (line_t *) malloc(sizeof(line_t));
 		cur->next->prev = cur;
-		printf("[%p|%p|%p]%s", cur->prev, cur, cur->next, cur->buf);
-		cur = cur->next;
-		if(!readline(cur->buf, fp))
+		if(!readline(cur->next->buf, fp))
 		{
-			cur = (line_t *) NULL;
-			printf("EOF reached, line_t: %luB", sizeof (line_t));
+			tail = cur->next;
+			cur = cur->prev;
+			tail->next = 0;
 		}
-		fflush(stdout); 
-	} else {
-		printf("Only down scrolling supported for now");
+		cur = cur->next;
+	}
+}
+int scroll(char lines, FILE * fp)
+{
+	if(lines > 0)
+	{
+		if(cur->next)
+		{
+			cur = cur->next;
+			printf("[%p|%p|%p]%s", cur->prev, cur, cur->next, cur->buf);
+			fflush(stdout);
+		}
+	} else if (lines < 0) {
+		if(cur->prev)
+		{
+			cur = cur->prev;
+			printf("[%p|%p|%p]%s", cur->prev, cur, cur->next, cur->buf);
+		} else { printf("At top\n");}
+		fflush(stdout);
+	} else if (lines == 0) {
+		printf("Can't scroll 0 lines.Sorry\n");
+		fflush(stdout);
 	}
 }
 int main(int argc, char ** argv){
@@ -49,20 +70,27 @@ int main(int argc, char ** argv){
 	
 	//setbuf(stdout, NULL);			// Disable buffering of stdout
 
-	line_t * root = malloc(sizeof(line_t));
+	root = malloc(sizeof(line_t));
 	root->prev = 0;
 	root->next = (line_t *)&cur;
 	
+	tail = (line_t *) NULL;
+
 	FILE * fp = fopen(argv[1] ? argv[1] : "file.default", "rw");
 	cur = root;
-	readline(cur->buf, fp);
+//	readline(cur->buf, fp);
+	loadfile(fp);
+	cur = root;
 	char c;
 	while(read(0, &c, 1) != -1)
 	{
-		if(c == 'j' && cur != (line_t *) NULL)
+		if(c == 'j' && cur != tail)
 		{
 			scroll(1, fp);
-		} else if (c == 'q') {
+		} else if (c == 'k' && cur != root) {
+			scroll(-1, fp);
+		}
+		else if (c == 'q') {
 			break;
 		}
 	}	
